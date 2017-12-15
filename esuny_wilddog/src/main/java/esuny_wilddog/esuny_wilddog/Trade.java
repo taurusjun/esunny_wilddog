902794f506d34db31e34d23f7bd9328152f35cf9
wuweitao <wuweitao@win7-32>
2017-12-15 18:06:52 +0800

    add Trade

---
+++ esuny_wilddog/src/main/java/esuny_wilddog/esuny_wilddog/Trade.java
@@ -0,0 +1,18 @@
+package esuny_wilddog.esuny_wilddog;
+
+/*
+ * 逐笔成交
+ * 
+ */
+public class Trade {
+	public String contract; // HKEX.HSI.1801
+	public String timestamp;
+	public double price;
+	public long volume;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
